3b66e7dbabf4866e48c2256ed982bb3e40e2e62a
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-01-24 20:20:55 -0600

    final update

--- Module1/Chapter 1 Tutorial/script.js
+++ Module1/Chapter 1 Tutorial/script.js
@@ -1,1 +1,15 @@
-...
+/* JavaScript 6th Edition
+    Chapter 1
+    Chapter case
+
+    Tinley Xeriscapes
+    Author: Sherouk Omara
+    Date: 1.24.21
+
+    Filename: script.js
+*/
+
+//define variables containing img src values - I'm using let and const to define variables since they are the new standard
+let blanket = "images/blanket.jpg";
+let bluestem = "images/bluestem.jpg";
+let rugosa = "images/rugosa.jpg";
