8f33647ece4e4271179e218d58a66fb25cebd3f0
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-03 13:32:42 -0600

    finished tutorial project

--- Module2/Chapter 2 Tutorial/ft.js
+++ Module2/Chapter 2 Tutorial/ft.js
@@ -10,17 +10,33 @@
  *    Filename: ft.js
  */
 
+let photographerCost = 0;
 let totalCost = 0;
 let memoryBook = false;
 let reproductionRights = false;
 
 // calculates all costs based on staff and adds to total cost
 function calcStaff () {
-    const num = document.getElementById("photognum");
-    const hrs = document.getElementById("photoghrs");
+    let num = document.getElementById("photognum");
+    let hrs = document.getElementById("photoghrs");
+    let distance = document.getElementById("distance");
     totalCost -= photographerCost;
-    photographerCost = num.value * 100 * hrs.value;
+    photographerCost = num.value * 100 * hrs.value + distance.value * num.value;
     totalCost += photographerCost;
+    document.getElementById("estimate").innerHTML = "$" + totalCost;
+};
+
+// adds/subtracts cost of memory book from total cost
+function toggleMembook () {
+    (document.getElementById("membook").checked === false) ?
+        totalCost -= 250 : totalCost += 250;
+    document.getElementById("estimate").innerHTML = "$" + totalCost;
+};
+
+// adds/subtracts cost of reproduction rights from total cost
+function toggleRights () {
+    (document.getElementById("reprodrights").checked === false) ?
+        totalCost -= 1250 : totalCost += 1250;
     document.getElementById("estimate").innerHTML = "$" + totalCost;
 };
 
@@ -31,16 +47,18 @@ function resetForm () {
     document.getElementById("membook").checked = memoryBook;
     document.getElementById("reprodrights").checked = reproductionRights;
     document.getElementById("distance").value = 0;
-    calcStaff ();
-    createEventListeners ();
+    calcStaff();
+    createEventListeners();
 };
 
 // resets form when page is reloaded
-document.addEventListener("load",resetForm,false);
+document.addEventListener("load", resetForm, false);
 
 // creates event listeners
 function createEventListeners () {
-    document.getElementById("photognum").addEventListener("change",calcStaff,false);
-    document.getElementById("photoghrs").addEventListener("change",calcStaff,false);
+    document.getElementById("photognum").addEventListener("change", calcStaff, false);
+    document.getElementById("photoghrs").addEventListener("change", calcStaff, false);
+    document.getElementById("membook").addEventListener("change", toggleMembook, false);
+    document.getElementById("reprodrights").addEventListener("change", toggleRights, false);
+    document.getElementById("distance").addEventListener("change", calcStaff, false);
 };
-
