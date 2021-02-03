3202a7e19b62c444aaadbfc7e4e9ff8c7a7f4b57
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-02 20:33:25 -0600

    halfway updates

--- Module2/Chapter 2 Tutorial/ft.js
+++ Module2/Chapter 2 Tutorial/ft.js
@@ -4,9 +4,43 @@
 
  *    Fan Trick Fine Art Photography
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author: Sherouk Omara
+ *    Date: 2.2.2021  
 
  *    Filename: ft.js
  */
+
+let totalCost = 0;
+let memoryBook = false;
+let reproductionRights = false;
+
+// calculates all costs based on staff and adds to total cost
+function calcStaff () {
+    const num = document.getElementById("photognum");
+    const hrs = document.getElementById("photoghrs");
+    totalCost -= photographerCost;
+    photographerCost = num.value * 100 * hrs.value;
+    totalCost += photographerCost;
+    document.getElementById("estimate").innerHTML = "$" + totalCost;
+};
+
+// sets all form field values to defaults
+function resetForm () {
+    document.getElementById("photognum").value = 1;
+    document.getElementById("photoghrs").value = 2;
+    document.getElementById("membook").checked = memoryBook;
+    document.getElementById("reprodrights").checked = reproductionRights;
+    document.getElementById("distance").value = 0;
+    calcStaff ();
+    createEventListeners ();
+};
+
+// resets form when page is reloaded
+document.addEventListener("load",resetForm,false);
+
+// creates event listeners
+function createEventListeners () {
+    document.getElementById("photognum").addEventListener("change",calcStaff,false);
+    document.getElementById("photoghrs").addEventListener("change",calcStaff,false);
+};
 
