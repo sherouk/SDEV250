cefa696b53782b7ea136cef356e3474af8e40d4a
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-07 16:28:58 -0600

    first update

---
+++ Module2/Chapter 3 Tutorial/tt.js
@@ -0,0 +1,15 @@
+/* JavaScript 6th Edition
+* Chapter 3
+* Chapter case
+* Tipton Turbines
+* Variables and functions
+* Author: Sherouk Omara
+* Date: 2/7/21
+* Filename: tt.js
+*/
+
+// global variables
+const daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const opponents = ["Lightning","Combines", "Combines", "Combines", "Lightning", "Lightning", "Lightning", "Lightning", "Barn Raisers", "Barn Raisers", "Barn Raisers", "Sodbusters", "Sodbusters", "Sodbusters", "Sodbusters", "(off)", "River Riders", "River Riders", "River Riders", "Big Dippers", "Big Dippers", "Big Dippers", "(off)", "Sodbusters", "Sodbusters", "Sodbusters", "Combines", "Combines", "Combines", "(off)", "(off)"];
+const gameLocation = ["away", "away", "away", "away", "home", "home", "home", "home", "home", "home", "home", "away", "away", "away", "away", "", "away", "away", "away", "away", "away", "away", "", "home", "home", "home", "home", "home", "home", "", ""];
+
