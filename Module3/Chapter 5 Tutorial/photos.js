c689f6c3728f69a88cf9346ad1ee65473f0b64d4
Sherouk O <73035812+sherouk@users.noreply.github.com>
2021-02-22 20:32:23 -0600

    Update photos.js

--- Module3/Chapter 5 Tutorial/photos.js
+++ Module3/Chapter 5 Tutorial/photos.js
@@ -10,20 +10,20 @@
  *    Filename: photos.js
  */
 
-"use strict"; // interpret document contents in JavaScript strict mode
+"use strict" // interpret document contents in JavaScript strict mode
 
 /* global variables */
-var photoOrder = [1,2,3,4,5];
+var photoOrder = [1,2,3,4,5]
 
 /* add src values to img elements based on order specified in photoOrder array*/
 function populateFigures() {
-   let filename;
-   let currentFig;
+   let filename
+   let currentFig
 
    for (let i = 1; i < 4; i++) {
-      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
-      currentFig = document.getElementsByTagName("img") [i - 1];
-      currentFig.src = filename;
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg"
+      currentFig = document.getElementsByTagName("img") [i - 1]
+      currentFig.src = filename
    }
 }
 
@@ -31,11 +31,11 @@ function populateFigures() {
 function rightArrow() {
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] + 1) === 6) {
-         photoOrder[i] = 1;
+         photoOrder[i] = 1
       } else {
-         photoOrder[i] += 1;
+         photoOrder[i] += 1
       }
-      populateFigures();
+      populateFigures()
    }
 }
 
@@ -43,12 +43,40 @@ function rightArrow() {
 function leftArrow() {
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] - 1) === 0) {
-         photoOrder[i] = 5;
+         photoOrder[i] = 5
       } else {
-         photoOrder[i] -= 1;
+         photoOrder[i] -= 1
       }
-      populateFigures();
+      populateFigures()
    }
+}
+
+/* switch to 5-image layout */
+function previewFive() {
+   let articleEl = document.getElementsByTagName("article") [0]
+   
+   //create figure and img elements for fifth image
+   let lastFigure = document.createElement("figure")
+   lastFigure.id = "fig5"
+   lastFigure.style.zIndex = "5"
+   lastFigure.style.position = "absolute"
+   lastFigure.style.right = "45px"
+   lastFigure.style.top = "67px"
+
+   let lastImage = document.createElement("img")
+   lastImage.width = "240"
+   lastImage.height = "135"
+
+   lastFigure.appendChild(lastImage)
+   articleEl.appendChild(lastFigure)
+
+   // clone figure element for fifth image and edit to be first image
+   let firstFigure = lastFigure.cloneNode(true)
+   firstFigure.id = "fig1"
+   firstFigure.style.right = ""
+   firstFigure.style.left = "45px"
+
+   articleEl.appendChild(firstFigure)
 }
 
 /* open center figure in separate window */
@@ -58,37 +86,44 @@ function zoomFig() {
 
 /* create event listeners for left arrow, right arrow, and center figure element*/
 function createEventListeners() {
-   const leftarrow = document.getElementById("leftarrow");
+   const leftarrow = document.getElementById("leftarrow")
    if (leftarrow.addEventListener) {
-      leftarrow.addEventListener("click", leftArrow, false);
+      leftarrow.addEventListener("click", leftArrow, false)
    } else if (leftarrow.attachEvent) {
-      leftarrow.attachEvent("onclick", leftArrow);
+      leftarrow.attachEvent("onclick", leftArrow)
    }
 
-   const rightarrow = document.getElementById("rightarrow");
+   const rightarrow = document.getElementById("rightarrow")
    if (rightarrow.addEventListener) {
-      rightarrow.addEventListener("click", rightArrow, false);
+      rightarrow.addEventListener("click", rightArrow, false)
    } else if (rightarrow.attachEvent) {
-      rightarrow.attachEvent("onclick", rightArrow);
+      rightarrow.attachEvent("onclick", rightArrow)
    }
 
-   const mainFig = document.getElementById("img") [1];
+   const mainFig = document.getElementById("img") [1]
    if (mainFig.addEventListener) {
-      mainFig.addEventListener("click", zoomFig, false);
+      mainFig.addEventListener("click", zoomFig, false)
    } else if (mainFig.attachEvent) {
-      mainFig.attachEvent("onclick", zoomFig);
+      mainFig.attachEvent("onclick", zoomFig)
+   }
+
+   const showAllButton = document.querySelector("#fiveButton p")
+   if (showAllButton.addEventListener) {
+      showAllButton.addEventListener("click", previewFive, false)
+   } else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent("onclick", previewFive)
    }
 }
 
 /* create event listeners and populate image elements */
 function setUpPage() {
-   createEventListeners();
-   populateFigures();
+   createEventListeners()
+   populateFigures()
 }
 
 /* run setUpPage() function when page finishes loading */
 if (window.addEventListener) {
-  window.addEventListener("load", setUpPage, false); 
+  window.addEventListener("load", setUpPage, false) 
 } else if (window.attachEvent)  {
-  window.attachEvent("onload", setUpPage);
+  window.attachEvent("onload", setUpPage)
 }
